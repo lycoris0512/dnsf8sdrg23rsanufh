547a9d0a937273f867c0da48a4ed9c55a6ee3668
NGUYEN TRUONG CUONG <nguyentruongcuong2k6@gmail.com>
2026-05-19 08:21:27 +0700

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,8 @@
+// Bạn hãy liệt kê tên tất cả các file nhạc của bạn vào đây.
+// Đặt file nhạc cùng thư mục với file này (thư mục gốc).
+// Tên file phải ghi chính xác (có cả đuôi .mp3) và đặt trong dấu ngoặc kép.
+
+window.MY_PLAYLIST = [
+    "1.mp3",
+	"2.mp3"
+];
